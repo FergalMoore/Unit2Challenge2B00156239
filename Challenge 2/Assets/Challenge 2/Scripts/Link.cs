f7e139c75e3850c96c4baf9757b9639da3579aa9
FergalMoore <B00156239@mytudublin.ie>
2023-10-15 22:41:01 +0100

    added everything

---
+++ Challenge 2/Assets/Challenge 2/Scripts/Link.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour
+{
+
+    public void OpenChannel()
+    {
+#if !UNITY_EDITOR
+		openWindow("https://www.youtube.com/watch?v=qqOqLNqAdDo");
+#endif
+    }
+
+    [DllImport("__Internal")]
+    private static extern void openWindow(string url);
+
+}
---
+++ Challenge 2/Assets/Challenge 2/Scripts/OpenLink.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void OpenChannel()
+    {
+
+        Application.OpenURL("https://www.youtube.com/watch?v=qqOqLNqAdDo");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
